ddfc928a494cdac3d1031b44fbb9bd4203d6ae38
Jaroslav Tyc <mail@jaroslavtyc.com>
2016-03-04 14:59:40 +0100

    Subscriber example extended by priority.

--- FOM/MauticSkeletonBundle/EventListener/WhyMeSubscriber.php
+++ FOM/MauticSkeletonBundle/EventListener/WhyMeSubscriber.php
@@ -9,13 +9,18 @@ class WhyMeSubscriber extends CommonSubscriber
 {
 
     /**
-     * @return array
+     * @see \Symfony\Component\EventDispatcher\EventSubscriberInterface::getSubscribedEvents for description.
+     * {@inheritdoc}
      */
     static public function getSubscribedEvents()
     {
         return [
-            UserEvents::USER_PRE_DELETE => 'iWarnYou',
-            UserEvents::USER_POST_DELETE => 'whoWillPayForThis'
+            UserEvents::USER_PRE_DELETE => 'iWarnYou', /* 0 is used as a default priority */
+            UserEvents::USER_POST_DELETE => ['whoWillPayForThis', 9999 /* high priority */],
+            UserEvents::USER_POST_DELETE => [
+                ['youWillPay', 9998 /* a little bit lesser priority */],
+                ['userOfYourCreationWill' /* 0 si used as a default priority */]
+            ]
         ];
     }
 
@@ -31,6 +36,16 @@ class WhyMeSubscriber extends CommonSubscriber
     public function whoWillPayForThis()
     {
         throw new \LogicException('Who will pay for forgotten skeleton code?');
+    }
+
+    public function youWillPay()
+    {
+        throw new \LogicException('You will pay for this forgotten skeleton code.');
+    }
+
+    public function userOfYourCreationWill()
+    {
+        throw new \LogicException('And what is worse, user of your creation will pay too for this forgotten skeleton code.');
     }
 
 }
